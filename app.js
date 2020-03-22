e8fa85ae84e24d53531a4171215077158bbc2bd3
pauljunkin <pjunkin@terpmail.umd.edu>
2020-03-22 16:10:51 -0400

    Initial Setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html' , function(error,data){
+        if(error){
+            res.writeHead(404)
+            res.write('error')
+        }
+        else{
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port, function(error){
+    if(error){
+        console.log('Somthing bla')
+    }
+    else{
+        console.log('gooda')
+    }
+})
